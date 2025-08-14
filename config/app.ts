ef421f631512ffa92a7cff187d548a8cf2eb12ed
Daniel Diaz <carlosdanielpinzadiaz@gmail.com>
2025-08-14 18:09:54 -0500

    w

--- config/app.ts
+++ config/app.ts
@@ -1,34 +1,22 @@
+// config/app.ts
 import env from '#start/env'
 import app from '@adonisjs/core/services/app'
 import { Secret } from '@adonisjs/core/helpers'
 import { defineConfig } from '@adonisjs/core/http'
 
-/**
- * The app key is used for encrypting cookies, generating signed URLs,
- * and by the "encryption" module.
- *
- * The encryption module will fail to decrypt data if the key is lost or
- * changed. Therefore it is recommended to keep the app key secure.
- */
 export const appKey = new Secret(env.get('APP_KEY'))
 
-/**
- * The configuration settings used by the HTTP server
- */
 export const http = defineConfig({
+  host: (env.get('HOST') || '0.0.0.0') as string,
+  port: Number(env.get('PORT') || 3333),
+
   generateRequestId: true,
   allowMethodSpoofing: false,
-
-  /**
-   * Enabling async local storage will let you access HTTP context
-   * from anywhere inside your application.
-   */
   useAsyncLocalStorage: false,
 
-  /**
-   * Manage cookies configuration. The settings for the session id cookie are
-   * defined inside the "config/session.ts" file.
-   */
+  // Conversión a booleano para TRUST_PROXY
+  trustProxy: (env.get('TRUST_PROXY') || 'false') === 'true',
+
   cookie: {
     domain: '',
     path: '/',
